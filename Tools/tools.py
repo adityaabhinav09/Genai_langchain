206de2ed0f1d932713a90328e031310e7229e4a5
adityaabhinav09 <adityaabhinav108@gmail.com>
2025-05-03 00:37:03 +0530

    Added Tools

--- Tools/tools.py
+++ Tools/tools.py
@@ -1,5 +1,12 @@
-print("Everything is Okay")
-from dotenv import dotenv_values
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-secrets = dotenv_values(".env")
-print(secrets)  # Shows all key-value pairs
+key = os.getenv('GEMINI_API_KEY')
+model = ChatGoogleGenerativeAI(model='gemini-1.5-pro',api_key=key)
+
+
+result = model.invoke("what is your name?")
+
+print(result.content) 
